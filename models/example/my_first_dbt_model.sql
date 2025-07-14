6a1c756d1df1e98c85b8ef1052e29fae0a4053f9
collier.matthew@gmail.com <collier.matthew@gmail.com>
2025-07-14 22:21:41 +0000

    Add index on new model-1 table.

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,7 +7,10 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+{{ config(
+    materialized='table',
+    indexes = [{'columns': ['id'], 'unique': True}]    
+) }}
 
 with source_data as (
 
